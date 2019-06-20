e9a88c8b7378ca6fdf9be3b0ad934edd9dc47e66
Salvador Hernández Mendoza <salvadorhm@utectulancingo.edu.mx>
2019-06-20 16:11:12 -0500

    update

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = '127.0.0.1'
-db_name = 'kuorra_login'
-db_user = 'kuorra'
-db_pw = 'kuorra.2018'
+db_host = 'z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'ewjs9gackau6q79q' # Database
+db_user = 'r5b9tus43ikka4kz' # Username
+db_pw = 'geujd68ew3btwya4' # Password
 
 db = web.database(
     dbn='mysql',
